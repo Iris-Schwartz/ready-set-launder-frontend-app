0cc55a1bd4cfa2a5ee773389871163c5629c9ba1
Iris-Kuzmenko <kuzmenkoiris@gmail.com>
2020-08-03 15:15:11 -0500

    in items new, styles group of wash setting images

--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,63 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Signup from '../views/Signup.vue'
-import Login from '../views/Login.vue'
-import Logout from '../views/Logout.vue'
-import ItemsIndex from '../views/ItemsIndex.vue'
-import ItemsNew from '../views/ItemsNew.vue'
-import ItemsLaunder from '../views/ItemsLaunder.vue'
-import UsersEdit from '../views/UsersEdit.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import ItemsIndex from "../views/ItemsIndex.vue";
+import ItemsNew from "../views/ItemsNew.vue";
+import ItemsLaunder from "../views/ItemsLaunder.vue";
+import UsersEdit from "../views/UsersEdit.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/signup',
-    name: 'signup',
-    component: Signup
+    path: "/signup",
+    name: "signup",
+    component: Signup,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: Login
+    path: "/login",
+    name: "login",
+    component: Login,
   },
   {
-    path: '/logout',
-    name: 'logout',
-    component: Logout
+    path: "/logout",
+    name: "logout",
+    component: Logout,
   },
   {
-    path: '/items',
-    name: 'items-index',
-    component: ItemsIndex
+    path: "/items",
+    name: "items-index",
+    component: ItemsIndex,
   },
   {
-    path: '/items/new',
-    name: 'items-new',
-    component: ItemsNew
+    path: "/items/new",
+    name: "items-new",
+    component: ItemsNew,
   },
   {
-    path: '/items/launder',
-    name: 'items-launder',
-    component: ItemsLaunder
+    path: "/items/launder",
+    name: "items-launder",
+    component: ItemsLaunder,
   },
   {
-    path: '/users/me/edit',
-    name: 'users-edit',
-    component: UsersEdit
-  }
-]
+    path: "/users/me/edit",
+    name: "users-edit",
+    component: UsersEdit,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/ItemsNew.vue
+++ src/views/ItemsNew.vue
@@ -1,38 +1,84 @@
 <template>
   <div class="items-new">
-    <form v-on:submit.prevent="createItem()">
-      <h2>Add Clothing Item to Inventory!</h2>
-      <ul>
-        <li v-for="error in errors" class="text-danger">{{ error }}</li>
-      </ul>
-      <div class="form-group">
-        <label>Item Name: </label>
-        <input type="text" v-model="name">
+    <!-- ======== @Region: #content ======== -->
+    <div id="content">
+      <div class="container">
+        <!-- Services -->
+        <h2 class="title-divider">
+          <span
+            >Add Clothing
+            <span class="font-weight-normal text-muted">Items</span></span
+          >
+          <small>Core features included in all plans.</small>
+        </h2>
+        <form v-on:submit.prevent="createItem()">
+          <ul>
+            <li v-for="error in errors" class="text-danger">{{ error }}</li>
+          </ul>
+          <div class="form-group">
+            <label>Item Name: </label>
+            <input type="text" v-model="name" />
+          </div>
+          <div class="form-group">
+            <label for="categories">Select Category: </label>
+            <select name="categories" id="categories" v-model="categoryId">
+              <option v-for="category in categories" :value="category.id">{{
+                category.name
+              }}</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <p>Select Wash Setting:</p>
+            <div class="row text-center mb-4">
+              <div class="col-lg-4 py-2" v-for="washSetting in washSettings">
+                <input
+                  type="radio"
+                  v-model="washSettingId"
+                  :id="washSetting.id"
+                  :value="washSetting.id"
+                />
+                <img
+                  :src="washSetting.image_url"
+                  style="width:48px;height:50px"
+                  class="text-primary"
+                  data-animate="fadeIn"
+                  data-animate-delay="0.1"
+                />
+
+                <h4 class="mt-2">
+                  {{ washSetting.name }}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div class="form-group">
+            <p>Select Dry Setting:</p>
+            <div class="row text-center mb-4">
+              <div class="col-lg-4 py-2" v-for="drySetting in drySettings">
+                <input
+                  type="radio"
+                  v-model="drySettingId"
+                  :id="drySetting.id"
+                  :value="drySetting.id"
+                />
+                <img
+                  :src="drySetting.image_url"
+                  style="width:48px;height:50px"
+                  class="text-primary"
+                  data-animate="fadeIn"
+                  data-animate-delay="0.1"
+                />
+
+                <h4 class="mt-2">
+                  {{ drySetting.name }}
+                </h4>
+              </div>
+            </div>
+          </div>
+          <input type="submit" class="bt btn-primary" value="Submit" />
+        </form>
       </div>
-      <div class="form-group">
-        <label for="categories">Select Category: </label>
-        <select name="categories" id="categories" v-model="categoryId">
-          <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <p>Select Wash Setting: </p>
-        <div v-for="washSetting in washSettings">
-          <input type="radio" v-model="washSettingId" :id="washSetting.id" :value="washSetting.id">
-          <img :src="washSetting.image_url" style="width:48px;height:50px">
-          <label :for="washSetting.name">{{ washSetting.name }}</label>
-        </div>
-      </div>
-      <div class="form-group">
-        <p>Select Dry Setting: </p>
-        <div v-for="drySetting in drySettings">
-          <input type="radio" v-model="drySettingId" :value="drySetting.id" :id="drySetting.id">
-           <img :src="drySetting.image_url" style="width:48px;height:50px">
-          <label :for="drySetting.name">{{ drySetting.name }}</label>
-        </div>
-      </div>
-      <input type="submit" class="bt btn-primary" value="Submit">
-    </form>
+    </div>
   </div>
 </template>
 
@@ -40,7 +86,7 @@
 import axios from "axios";
 
 export default {
-  data: function () {
+  data: function() {
     return {
       name: "",
       washSettingId: null,
@@ -52,7 +98,7 @@ export default {
       errors: [],
     };
   },
-  created: function () {
+  created: function() {
     axios.get("/api/categories").then((response) => {
       console.log(response.data);
       this.categories = response.data;
@@ -67,7 +113,7 @@ export default {
     });
   },
   methods: {
-    createItem: function () {
+    createItem: function() {
       var params = {
         name: this.name,
         category_id: this.categoryId,
