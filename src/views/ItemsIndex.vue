efe7459699e4de84dfc091ed80a05e70f7b932e2
Iris-Kuzmenko <kuzmenkoiris@gmail.com>
2020-08-04 10:41:18 -0500

    in ItemsIndex view, places items in a list group within a card

--- src/views/ItemsIndex.vue
+++ src/views/ItemsIndex.vue
@@ -1,31 +1,39 @@
 <template>
   <div class="items-index">
-    <h2>Select Items to Wash!</h2>
-    <form v-on:submit.prevent="updateStatus()">
-      <select v-model="categoryFilter">
-        <option value="">All Categories</option>
-        <option v-for="category in categories" :value="category.name">{{
-          category.name
-        }}</option>
-      </select>
-      <div
-        v-for="item in orderBy(
-          filterBy(items, categoryFilter, 'category_name'),
-          'name'
-        )"
-      >
-        <input
-          type="checkbox"
-          :id="item.id"
-          :value="item.id"
-          v-model="itemIds"
-        />
-        <label :for="item.name">{{ item.name }}</label>
-      </div>
-      <br />
-      <br />
-      <input type="submit" class="btn btn-primary" value="Done" />
-    </form>
+    <div class="container">
+      <h2>Select Items to Wash!</h2>
+      <form v-on:submit.prevent="updateStatus()">
+        <div class="card">
+          <h4 class="card-header">
+            <select v-model="categoryFilter">
+              <option value="">All Categories</option>
+              <option v-for="category in categories" :value="category.name">
+                {{ category.name }}
+              </option>
+            </select>
+          </h4>
+          <ul class="list-group list-group-flush">
+            <div
+              v-for="item in orderBy(
+                filterBy(items, categoryFilter, 'category_name'),
+                'name'
+              )"
+            >
+              <li class="list-group-item">
+                <input
+                  type="checkbox"
+                  :id="item.id"
+                  :value="item.id"
+                  v-model="itemIds"
+                />
+                <label :for="item.name">{{ item.name }}</label>
+              </li>
+            </div>
+          </ul>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Done" />
+      </form>
+    </div>
   </div>
 </template>
 
