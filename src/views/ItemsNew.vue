01ec9113ccdccef0aabb05486e703dfe824f6ad9
Iris-Kuzmenko <kuzmenkoiris@gmail.com>
2020-08-04 19:15:21 -0500

    updates styling in ItemsNew view

--- src/views/ItemsNew.vue
+++ src/views/ItemsNew.vue
@@ -1,99 +1,120 @@
 <template>
   <div class="items-new">
-    <ul>
-      <li v-for="error in errors" class="text-danger">{{ error }}</li>
-    </ul>
-    <form v-on:submit.prevent="createItem()">
-      <div class="form-group">
-        <label>Item Name: </label>
-        <input type="text" v-model="name" />
-      </div>
-      <div class="form-group">
-        <label for="categories">Select Category: </label>
-        <select name="categories" id="categories" v-model="categoryId">
-          <option v-for="category in categories" :value="category.id">{{
-            category.name
-          }}</option>
-        </select>
-      </div>
-      <div class="form-group">
-        Select Wash Setting:
-        <div
-          class="mt-4 owl-nav-over owl-nav-over-lg"
-          data-toggle="owl-carousel"
-          data-owl-carousel-settings='{"responsive":{"0":{"items":1}, "600":{"items":2}, "980":{"items":4}}, "margin":10, "nav":true, "dots":false}'
-        >
-          <div class="row text-center mb-4">
-            <div class="col-lg-4 py-2" v-for="washSetting in washSettings">
-              <div class="form-group">
-                <div class="card product-card overlay-hover">
-                  <div
-                    class="overlay-hover-content overlay-op-7 product-card-hover-tools"
-                  >
-                    <h7 class="text-white">
-                      <input
-                        type="radio"
-                        v-model="washSettingId"
-                        :id="washSetting.id"
-                        :value="washSetting.id"
-                      />
-                      {{ washSetting.name }}
-                    </h7>
-                  </div>
-                  <div class="pos-relative">
-                    <img
-                      class="card-img-top img-fluid"
-                      :src="washSetting.image_url"
-                      style="width:48px;height:50px"
-                      alt="Card image cap"
-                    />
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card product-card mb-4">
+          <div class="card-body p-4 pos-relative">
+            <h2 class="card-title mb-2">
+              Add Item to Clothing Inventory!
+            </h2>
+            <hr class="my-3" />
+            <ul>
+              <li v-for="error in errors" class="text-danger">{{ error }}</li>
+            </ul>
+
+            <div class="product-options row mb-4">
+              <form v-on:submit.prevent="createItem()">
+                <div class="form-group">
+                  <div class="col-md-12 text-sm text-uppercase">Item Name</div>
+                  <div class="col-md-4 mb-3">
+                    <input type="text" v-model="name" />
                   </div>
                 </div>
-              </div>
+                <div class="form-group">
+                  <div class="col-md-12 text-sm text-uppercase">Category</div>
+                  <div class="col-md-4 mb-3">
+                    <select
+                      name="categories"
+                      id="categories"
+                      v-model="categoryId"
+                    >
+                      <option
+                        v-for="category in categories"
+                        :value="category.id"
+                        >{{ category.name }}</option
+                      >
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-md-12 text-sm text-uppercase">
+                    Select Wash Setting
+                  </div>
+                  <div class="col-md-12 col-lg-12 mb-3">
+                    <div class="row text-center mb-4">
+                      <div class="col-sm-4" v-for="washSetting in washSettings">
+                        <div class="form-group">
+                          <div class="card product-card overlay-hover">
+                            <div
+                              class="overlay-hover-content overlay-op-7 product-card-hover-tools"
+                            >
+                              <h6 class="text-white">
+                                <input
+                                  type="radio"
+                                  v-model="washSettingId"
+                                  :id="washSetting.id"
+                                  :value="washSetting.id"
+                                />
+                                {{ washSetting.name }}
+                              </h6>
+                            </div>
+                            <div class="pos-relative">
+                              <img
+                                class="card-img-top img-fluid"
+                                :src="washSetting.image_url"
+                                style="width:48px;height:50px"
+                                alt="Card image cap"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-md-12 text-sm text-uppercase">
+                    Select Dry Setting
+                  </div>
+                  <div class="col-md-12 col-lg-12 mb-3">
+                    <div class="row text-center mb-4">
+                      <div class="col-sm-4" v-for="drySetting in drySettings">
+                        <div class="form-group">
+                          <div class="card product-card overlay-hover">
+                            <div
+                              class="overlay-hover-content overlay-op-7 product-card-hover-tools"
+                            >
+                              <h6 class="text-white">
+                                <input
+                                  type="radio"
+                                  v-model="drySettingId"
+                                  :id="drySetting.id"
+                                  :value="drySetting.id"
+                                />
+                                {{ drySetting.name }}
+                              </h6>
+                            </div>
+                            <div class="pos-relative">
+                              <img
+                                class="card-img-top img-fluid"
+                                :src="drySetting.image_url"
+                                style="width:48px;height:50px"
+                                alt="Card image cap"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <input type="submit" class="bt btn-primary" value="Submit" />
+              </form>
             </div>
           </div>
         </div>
       </div>
-      <div class="form-group">
-        Select Dry Setting:
-        <div
-          class="mt-4 owl-nav-over owl-nav-over-lg"
-          data-toggle="owl-carousel"
-          data-owl-carousel-settings='{"responsive":{"0":{"items":1}, "600":{"items":2}, "980":{"items":4}}, "margin":10, "nav":true, "dots":false}'
-        >
-          <div class="row text-center mb-4">
-            <div class="col-lg-4 py-2" v-for="drySetting in drySettings">
-              <div class="form-group">
-                <div class="card product-card overlay-hover">
-                  <div
-                    class="overlay-hover-content overlay-op-7 product-card-hover-tools"
-                  >
-                    <h7 class="text-white">
-                      <input
-                        type="radio"
-                        v-model="drySettingId"
-                        :id="drySetting.id"
-                        :value="drySetting.id"
-                      />
-                      {{ drySetting.name }}
-                    </h7>
-                  </div>
-                  <div class="pos-relative">
-                    <img
-                      class="card-img-top img-fluid"
-                      :src="drySetting.image_url"
-                      style="width:48px;height:50px"
-                      alt="Card image cap"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <input type="submit" class="bt btn-primary" value="Submit" />
-    </form>
+    </div>
   </div>
 </template>
 
